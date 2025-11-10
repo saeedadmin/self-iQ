40e6f5c3ebb7bf024eb4c7ccc244de16209db61f
saeed <saeedmrpg@gmail.com>
2025-11-10 23:00:02 +0330

    Fix bootstrap path for bot runtime

--- src/Bot/run.php
+++ src/Bot/run.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use App\Bot\BaseHandler;
 use App\Config\SettingsFactory;
 
-require_once __DIR__ . '/../config/bootstrap.php';
+require_once dirname(__DIR__, 2) . '/config/bootstrap.php';
 
 $sessionFile = getenv('SESSION_FILE');
 if ($sessionFile === false || $sessionFile === '') {
