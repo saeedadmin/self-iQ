4d35b5cc0589ce483d641e946464d16c4f18277f
saeed <saeedmrpg@gmail.com>
2025-11-12 20:04:32 +0330

    Seed attribute shim into vendor during composer bootstrap

--- scripts/composer-bootstrap.php
+++ scripts/composer-bootstrap.php
@@ -7,3 +7,15 @@ $overrides = __DIR__ . '/../src/Liveproto/overrides.php';
 if (is_file($overrides)) {
     require_once $overrides;
 }
+
+$shimSource = __DIR__ . '/../src/Liveproto/Attributes/AttributesEngine.php';
+$shimTarget = __DIR__ . '/../vendor/taknone/attributes/src/AttributesEngine.php';
+
+if (is_file($shimSource) && !is_file($shimTarget)) {
+    $shimDirectory = dirname($shimTarget);
+    if (!is_dir($shimDirectory)) {
+        mkdir($shimDirectory, 0777, true);
+    }
+
+    copy($shimSource, $shimTarget);
+}
--- src/Liveproto/overrides.php
+++ src/Liveproto/overrides.php
@@ -25,6 +25,7 @@ $attributesShim = __DIR__ . '/Attributes/AttributesEngine.php';
 if (is_file($attributesShim) && !trait_exists('Tak\\Attributes\\AttributesEngine', false)) {
     require $attributesShim;
 }
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/Attributes');
 
 final class AttributeShim
 {
