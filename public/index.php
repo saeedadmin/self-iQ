f7acf40db6877b88b7aa23e6485478d72f38ee03
saeed <saeedmrpg@gmail.com>
2025-11-14 21:09:27 +0330

    Start sessions before output to avoid header warnings

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 use App\Http\Kernel;
 
+if (PHP_SAPI !== 'cli' && session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
 require_once dirname(__DIR__) . '/config/bootstrap.php';
 
 $kernel = new Kernel();
